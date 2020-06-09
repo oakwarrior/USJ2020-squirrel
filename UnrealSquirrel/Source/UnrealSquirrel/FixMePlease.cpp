7a9819066bab5b0bc02560ee4d97378dd063fca9
Simon <simon.otanu@gmail.com>
2020-06-09 21:39:31 +0200

    merge

---
+++ UnrealSquirrel/Source/UnrealSquirrel/FixMePlease.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FixMePlease.h"
+
+FixMePlease::FixMePlease()
+{
+}
+
+FixMePlease::~FixMePlease()
+{
+}
